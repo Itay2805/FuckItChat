acab0bc67d54ecdf7ba84ba1d4b4d8017b4c5d93
BuildTools <colorlord98@gmail.com>
2020-08-28 13:26:24 +0300

    Server UI now uses Server to actually connect

--- FuckItServer/FuckItServer.cpp
+++ FuckItServer/FuckItServer.cpp
@@ -4,4 +4,16 @@ FuckItServer::FuckItServer(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	connect(ui.actionStart_Server, &QAction::triggered, this, &FuckItServer::StartServer);
+
+
+}
+
+void FuckItServer::StartServer() {
+	ServerStartDialog dialog(this);
+	if (dialog.exec() == QDialog::Accepted) {
+		server = new Server(this);
+		server->Start(dialog.GetPort(), dialog.GetSalt(), dialog.GetPassword());
+	}
 }
--- FuckItServer/FuckItServer.h
+++ FuckItServer/FuckItServer.h
@@ -2,14 +2,20 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_FuckItServer.h"
+#include "ServerStartDialog.h"
+#include "Server.h"
 
 class FuckItServer : public QMainWindow
 {
 	Q_OBJECT
-
+	
+private:
+	Server* server;
 public:
 	FuckItServer(QWidget *parent = Q_NULLPTR);
 
 private:
 	Ui::FuckItServerClass ui;
+
+	void StartServer();
 };
